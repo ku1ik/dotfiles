3e9a7b3e0e1a974d1bdaec31efe4b0202dd665ff
Marcin Kulik <marcin.kulik@llp.pl>
2009-10-29 15:18:34 +0100

    immediately insert first match

--- completion.zsh
+++ completion.zsh
@@ -6,14 +6,19 @@ compinit
 zmodload zsh/complist
 
 setopt complete_in_word
+setopt MENU_COMPLETE # immediatelly insert first match
 
+# Processes
 zstyle ':completion:*:*:*:*:processes' menu yes select
 zstyle ':completion:*:*:*:*:processes' force-list always
 zstyle ':completion:*:processes' command 'ps -ux'
+
 zstyle ':completion:*' users off
 zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
-zstyle ':completion:*:descriptions' format '%U%B%d%b%u'
+# zstyle ':completion:*:descriptions' format '%U%B%d%b%u'
 zstyle ':completion:*:warnings' format '%BSorry, no matches for: %d%b'
+
+zstyle ':completion:*' accept-exact '*(N)'
 
 # zstyle ':completion:*:processes-names' command  'ps c -au ${USER} -o command | uniq'
 # zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#)*=0=01;31'
