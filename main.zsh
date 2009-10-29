650bebb34b24d9726480a3ebe90d8067cfc3e83c
Marcin Kulik <marcin.kulik@llp.pl>
2009-10-29 11:04:48 +0100

    removed prompt.zsh, zsh extensions to all files

---
+++ main.zsh
@@ -0,0 +1,17 @@
+setopt nohup
+setopt nobeep
+setopt extendedglob
+setopt NOBGNICE
+setopt autocd
+
+. ~/.zsh/completion.zsh
+. ~/.zsh/key-bindings.zsh
+. ~/.zsh/aliases.zsh
+. ~/.zsh/exports.zsh
+. ~/.zsh/colors.zsh
+. ~/.zsh/git.zsh
+. ~/.zsh/termsupport.zsh
+. ~/.zsh/rake_completion.zsh
+. ~/.zsh/misc.zsh
+. ~/.zsh/history.zsh
+
