67f835943994f094a8d0c73702240a14809674bd
Marcin Kulik <marcin.kulik@gmail.com>
2009-11-07 19:11:39 +0100

    fixed ctrl-left/right under Konsole

--- key-bindings.zsh
+++ key-bindings.zsh
@@ -4,16 +4,24 @@ bindkey "^[[F"  end-of-line
 bindkey "^[[4~" end-of-line
 bindkey ' ' magic-space    # also do history expansion on space
 
-bindkey '^[^[[D' backward-word
-bindkey '^[Od' backward-word
-bindkey ';5D' backward-word
-bindkey ';3D' backward-word
-bindkey '^[^[[C' forward-word
-bindkey '^[Oc' forward-word
-bindkey ';5C' forward-word
-bindkey ';3C' forward-word
-
-# bindkey "e[5D" backward-word
-# bindkey "ee[D" backward-word
-
 bindkey '^[[3~' delete-char
+
+bindkey '\e[1;5C' forward-word        # ctrl right
+bindkey '\e[1;5D' backward-word       # ctrl left
+
+#bindkey '^[^[[D' backward-word
+#bindkey '^[Od' backward-word
+#bindkey ';5D' backward-word
+#bindkey ';3D' backward-word
+#bindkey '^[^[[C' forward-word
+#bindkey '^[Oc' forward-word
+#bindkey ';5C' forward-word
+#bindkey ';3C' forward-word
+
+#bindkey '\eOc'    forward-word        # ctrl right
+#bindkey '\eOd'    backward-word       # ctrl left
+
+
+#bindkey "\e[5D" backward-word
+#bindkey "\ee[D" backward-word
+
