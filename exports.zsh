1ba89265e22c7da461a4b59463b20672c334a744
Marcin Kulik <marcin.kulik@gmail.com>
2010-09-02 16:13:23 +0200

    Use default pager for git

--- exports.zsh
+++ exports.zsh
@@ -4,7 +4,6 @@ export SCALA_HOME=/usr/share/scala
 export PATH="$HOME/bin:$PATH"
 export PATH="$PATH:$JAVA_HOME/bin"
 export PATH="$PATH:/usr/local/bin/"
-export GIT_PAGER=""
 export EDITOR=/usr/bin/vim
 export RUBYOPT=rubygems
 export BROWSER=chromium
