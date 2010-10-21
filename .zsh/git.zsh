8d6abbe51cabefb9b9b530cbf4006943201fc882
Marcin Kulik <marcin.kulik@gmail.com>
2010-10-21 23:18:54 +0200

    [zsh] improved git prompt a little

--- .zsh/git.zsh
+++ .zsh/git.zsh
@@ -1,44 +1,87 @@
 function git(){hub $@}
 
-export __ZSH_GIT_BRANCH=
-export __ZSH_GIT_INVALIDATE=1
-
 typeset -ga preexec_functions
 typeset -ga precmd_functions
 typeset -ga chpwd_functions
 
-git_parse_branch() {
-  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1) /'
+update_current_git_vars() {
+  unset __CURRENT_GIT_BRANCH
+  unset __CURRENT_GIT_BRANCH_STATUS
+  unset __CURRENT_GIT_BRANCH_IS_DIRTY
+
+  local st="$(git status 2>/dev/null)"
+  if [[ -n "$st" ]]; then
+    local -a arr
+    arr=(${(f)st})
+
+    if [[ $arr[1] =~ 'Not currently on any branch.' ]]; then
+      __CURRENT_GIT_BRANCH='no-branch'
+    else
+      __CURRENT_GIT_BRANCH="${arr[1][(w)4]}";
+    fi
+
+    if [[ $arr[2] =~ 'Your branch is' ]]; then
+      if [[ $arr[2] =~ 'ahead' ]]; then
+        __CURRENT_GIT_BRANCH_STATUS='ahead'
+      elif [[ $arr[2] =~ 'diverged' ]]; then
+        __CURRENT_GIT_BRANCH_STATUS='diverged'
+      else
+        __CURRENT_GIT_BRANCH_STATUS='behind'
+      fi
+    fi
+
+    if [[ ! $st =~ 'nothing to commit' ]]; then
+      __CURRENT_GIT_BRANCH_IS_DIRTY='1'
+    fi
+  fi
 }
 
-git_compute_vars() {
-  export __ZSH_GIT_BRANCH="$(git_parse_branch)"
-  export __ZSH_GIT_INVALIDATE=
+prompt_git_info() {
+  if [ -n "$__CURRENT_GIT_BRANCH" ]; then
+    local s="("
+    s+="$__CURRENT_GIT_BRANCH"
+    case "$__CURRENT_GIT_BRANCH_STATUS" in
+      ahead)
+      s+="↑"
+      ;;
+      diverged)
+      s+="↕"
+      ;;
+      behind)
+      s+="↓"
+      ;;
+    esac
+    if [ -n "$__CURRENT_GIT_BRANCH_IS_DIRTY" ]; then
+      s+="⚡"
+    fi
+    s+=")"
+
+    printf " %s%s" "%{${fg[yellow]}%}$s%{$reset_color%}"
+  fi
 }
 
-# on each chdir update the cached git variable(s)
-preexec_functions+='git_preexec_update_vars'
-chpwd_functions+='git_chpwd_update_vars'
-precmd_functions+='git_precmd_update_vars'
-
-git_precmd_update_vars() {
-  test -n "$__ZSH_GIT_INVALIDATE" && git_compute_vars
-}
-
-git_chpwd_update_vars() {
-  export __ZSH_GIT_INVALIDATE=1
-}
-
-git_preexec_update_vars() {
-  case "$(history $HISTCMD)" in 
-    *git\ *)  export __ZSH_GIT_INVALIDATE=1 ;;
-    *g\ *)  export __ZSH_GIT_INVALIDATE=1 ;;
+preexec_update_git_vars() {
+  case "$1" in
+    git\ *)
+    __EXECUTED_GIT_COMMAND=1
+    ;;
+    g\ *)
+    __EXECUTED_GIT_COMMAND=1
+    ;;
   esac
 }
 
-git_prompt_info() {
-  echo $__ZSH_GIT_BRANCH
+precmd_update_git_vars() {
+  if [ -n "$__EXECUTED_GIT_COMMAND" ]; then
+    update_current_git_vars
+    unset __EXECUTED_GIT_COMMAND
+  fi
 }
 
-#git_compute_vars
+chpwd_update_git_vars() {
+  update_current_git_vars
+}
 
+preexec_functions+='preexec_update_git_vars'
+precmd_functions+='precmd_update_git_vars'
+chpwd_functions+='chpwd_update_git_vars'
