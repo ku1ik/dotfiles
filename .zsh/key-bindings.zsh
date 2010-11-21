07849e61d22c0bab7f715d3e9a3f6aec9f0f2df3
Marcin Kulik <marcin.kulik@gmail.com>
2010-11-21 12:37:55 +0100

    [zsh] updated keybindings for (tcsh-)backward-(delete-)word

--- .zsh/key-bindings.zsh
+++ .zsh/key-bindings.zsh
@@ -21,32 +21,35 @@ bindkey '^[[4~' end-of-line
 
 bindkey '^[[3~' delete-char
 bindkey "^[[3;3~" delete-word
+bindkey "^[" delete-word
 
-bindkey '\e[1;3D' backward-word
-bindkey '^[^[[D' backward-word # tmux
-bindkey '\e[1;3C' forward-word
-bindkey '^[^[[C' forward-word # tmux
+bindkey '\e[1;5D' backward-word # urxvt, konsole
+bindkey '^[OD' backward-word # tmux
+
+bindkey '\e[1;5C' forward-word # urxvt, konsole
+bindkey '^[OC' forward-word # tmux
+
+bindkey "^H" backward-delete-word # urxvt
 
 tcsh-backward-word () {
   local WORDCHARS="${WORDCHARS:s#/#}"
   zle emacs-backward-word
 }
 zle -N tcsh-backward-word
-bindkey '\e[1;5D' tcsh-backward-word # urxvt, konsole
-bindkey '^[OD' tcsh-backward-word # tmux
+bindkey '\e[1;3D' tcsh-backward-word
+bindkey '^[^[[D' tcsh-backward-word # tmux
 
 tcsh-forward-word () {
   local WORDCHARS="${WORDCHARS:s#/#}"
   zle emacs-forward-word
 }
 zle -N tcsh-forward-word
-bindkey '\e[1;5C' tcsh-forward-word # urxvt, konsole
-bindkey '^[OC' tcsh-forward-word # tmux
+bindkey '\e[1;3C' tcsh-forward-word
+bindkey '^[^[[C' tcsh-forward-word # tmux
 
 tcsh-backward-delete-word () {
   local WORDCHARS="${WORDCHARS:s#/#}"
   zle backward-delete-word
 }
 zle -N tcsh-backward-delete-word
-bindkey "^H" tcsh-backward-delete-word # urxvt
-# bindkey "\\e^H" tcsh-backward-delete-word # konsole
+bindkey "^[^?" tcsh-backward-delete-word # urxvt
