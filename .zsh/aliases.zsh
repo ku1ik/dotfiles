ea2c69b3d0e39ec3bed3f1fa06a2061344882dcb
Marcin Kulik <marcin.kulik@gmail.com>
2011-04-03 22:58:45 +0200

    Update aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,7 +1,10 @@
-alias l='ls -lah --color=auto --file-type'
+alias ls='ls -ahF --color=auto'
+alias ll='ls -l'
+# alias ll='ls -ltc --color=auto | head'
 # alias lsa='ls -la --color=auto'
-alias ll='ls -ltc --color=auto | head'
-alias grep='grep --color'
+alias ..='cd ..;' # can then do .. .. .. to move up multiple directories.
+alias ...='.. ..'
+alias cd-='cd;cd -'
 alias sk='ssh -l kill sickill.net'
 alias deneb='ssh -l rubydev deneb.llp.pl'
 alias deneb-admin='ssh -l llpadmin deneb.llp.pl'
@@ -10,9 +13,17 @@ alias psg='ps ax | grep'
 alias psug='ps x | grep'
 alias rtorrent='tmux attach -t rtorrent || tmux new -s rtorrent rtorrent'
 alias tailf='tail -f -n200'
+alias df='df -h'
+alias open='xdg-open'
 alias _='sudo'
 
+alias a='sudo apt-get'
+
+alias v=vim
 alias e=gvim
+alias et='gvim --remote-tab'
+
+alias x='dtrx -v'
 
 alias g='git'
 alias ga='git add'
@@ -59,7 +70,8 @@ alias t='todo.sh'
 alias tmux='tmux -2'
 alias ducks='du -cks * | sort -rn | head -11' # Lists the size of all the folders and files
 
-alias jsonpretty='ruby -rap -rjson -e "ap JSON.parse(STDIN.read)"'
+# alias jsonpretty='ruby -rap -rjson -e "ap JSON.parse(STDIN.read)"'
+alias jsonpretty='python -mjson.tool'
 
 alias cls="echo -ne '\033c'"
 
@@ -67,6 +79,9 @@ alias dl='cd ~/Downloads'
 
 alias -s -- jar='java -jar'
 
-alias v=vagrant
+alias vg=vagrant
 
 alias unsvn='find . -name .svn -print0 | xargs -0 rm -rf'
+
+# useful command to find what you should be aliasing:
+alias profileme="history | awk '{print \$2}' | awk 'BEGIN{FS=\"|\"}{print \$1}' | sort | uniq -c | sort -n | tail -n 20 | sort -nr"
