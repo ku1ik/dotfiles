a017aa9961530b51121a2151374540066b87a019
Marcin Kulik <marcin.kulik@gmail.com>
2011-04-12 21:12:21 +0200

    [xmonad] small tweaks

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -6,6 +6,8 @@ import XMonad.Layout.NoBorders
 import XMonad.Layout.PerWorkspace
 import XMonad.Layout.IM
 import XMonad.Layout.Grid
+import XMonad.Layout.Magnifier
+import XMonad.Layout.MouseResizableTile
 import XMonad.Actions.WindowGo
 import XMonad.Actions.CycleWS
 import XMonad.Hooks.UrgencyHook
@@ -18,11 +20,19 @@ import qualified XMonad.StackSet as W
 {- curLayout :: X String-}
 {- curLayout = gets windowset >>= return . description . W.layout . W.workspace . W.current-}
 
+{- http://hpaste.org/44583/xmonad_layout_rotation-}
+rotate :: LayoutClass layout a => layout a -> Int -> X (Maybe (layout a))
+rotate l n | n <= 0    = return (Just l)
+           | otherwise = do ml' <- rotate l (n - 1)
+                            case ml' of
+                              Nothing -> return Nothing
+                              Just l' -> handleMessage l' (SomeMessage NextLayout)
+
 myWorkspaces = ["1", "2", "3", "4", "5"]
 
 myLayout = desktopLayoutModifiers
            $ smartBorders
-           $ onWorkspace "2" (Tall 1 0.1 0.66 ||| Mirror(Tall 1 0.1 0.66) ||| Full)
+           $ onWorkspace "2" (Tall 1 0.1 0.66 ||| Mirror(Tall 1 0.1 0.66) ||| Full ||| mouseResizableTile)
            $ onWorkspace "4" imLayout
            $ baseLayout
   where
@@ -39,6 +49,7 @@ myKeys =
     , ("M-S-r", spawn "xmonad --recompile; xmonad --restart")
     , ("M-q", kill)
     , ("M-u", focusUrgent)
+    , ("M-o", toggleWS)
     {- , ("M-<Space>", sendMessage NextLayout >> (curLayout >>= \d->spawn $"notify-send "++d))-}
     , ("M-C-j", prevScreen)
     , ("M-C-k", nextScreen)
@@ -48,22 +59,29 @@ myKeys =
 myManageHook = composeAll . concat $
    [ [ isDialog --> doCenterFloat ]
    , [ className =? "Gvim" --> viewShift "3" ]
+   , [ className =? "Empathy" --> viewShift "4" ]
+   , [ className =? "Skype" --> viewShift "4" ]
+   , [ className =? "Screenkey" --> doF W.focusDown ]
    {- , [ className =? "Gajim.py"    --> doShift "jabber" ]-}
    {- , [(className =? "Google-chrome" <&&> resource =? "Dialog") --> doFloat]-}
      -- using list comprehensions and partial matches
    , [ className =? c --> doFloat | c <- myFloatsC ]
+   , [ className =? c --> doCenterFloat | c <- myCenteredFloatsC ]
    , [ title =? t --> doFloat | t <- myFloatsT ]
    , [ fmap ( c `isInfixOf`) className --> doFloat | c <- myMatchAnywhereFloatsC ]
    , [ fmap ( c `isInfixOf`) title     --> doFloat | c <- myMatchAnywhereFloatsT ]
    ]
    -- in a composeAll hook, you'd use: fmap ("VLC" `isInfixOf`) title --> doFloat
-  where myFloatsC = ["MPlayer", "Xmessage", "Gimp"]
+  where myFloatsC = ["MPlayer", "Xmessage", "Gimp", "Screenkey"]
+        myCenteredFloatsC = ["Gcalctool"]
         myFloatsT = ["Google Chrome Preferences"]
         myMatchAnywhereFloatsC = []
         myMatchAnywhereFloatsT = ["VLC"]
         viewShift = doF . liftM2 (.) W.greedyView W.shift
 
 main = do
+  {- http://hpaste.org/44586/modified_xmonadhs_with_layout-}
+  {- lay1 <- rotate myLayout 1-}
   xmonad $ withUrgencyHook NoUrgencyHook gnomeConfig
     { modMask = mod4Mask
     , focusedBorderColor = "#f07746"
