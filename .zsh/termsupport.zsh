2378c83a95c4631495c2ef8f231f62e47fa256b7
Marcin Kulik <marcin.kulik@gmail.com>
2011-11-05 23:36:39 +0100

    [zsh] enable auto-term-title for non-screen-like $TERM

--- .zsh/termsupport.zsh
+++ .zsh/termsupport.zsh
@@ -1,22 +1,25 @@
 case "$TERM" in
   xterm*|rxvt*)
-    # preexec () {
-    #   print -Pn "\e]0;%~: $1\a"  # xterm
-    # }
+    preexec () {
+      print -Pn "\e]0;%~: $1\a"  # xterm
+    }
+
     precmd () {
       print -Pn "\e]0;%~\a"  # xterm
       # echo -ne "\a"
     }
     ;;
+
   screen*)
-    preexec () {
-      local CMD=${1[(wr)^(*=*|sudo|ssh|-*)]}
-      echo -ne "\ek$CMD\e\\"
-      print -Pn "\e]0;%~: $1\a"  # xterm
-    }
-    precmd () {
-      echo -ne "\ekzsh\e\\"
-      print -Pn "\e]0;%~\a"  # xterm
-    }
+    # preexec () {
+    #   local CMD=${1[(wr)^(*=*|sudo|ssh|-*)]}
+    #   echo -ne "\ek$CMD\e\\"
+    #   print -Pn "\e]0;%~: $1\a"  # xterm
+    # }
+
+    # precmd () {
+    #   echo -ne "\ekzsh\e\\"
+    #   print -Pn "\e]0;%~\a"  # xterm
+    # }
     ;;
 esac
