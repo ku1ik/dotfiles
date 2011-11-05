b0f782972e03085b0a76c14405fcaeaeb67d56f4
Marcin Kulik <marcin.kulik@gmail.com>
2011-11-05 19:03:19 +0100

    [zsh] transient right prompt

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -5,22 +5,60 @@ autoload -Uz promptinit
 promptinit
 
 # Erase the right prompt after a line is accepted
-# setopt transientrprompt
+setopt transientrprompt
 
 # Print the exit value for commands with non-zero exit status
-# setopt printexitvalue
+setopt printexitvalue
 
 setopt prompt_subst
 
-export PROMPT='
-${SSH_TTY+"%n@%m:"}%3~$(prompt_git_info)$(rvm_info_for_prompt)
-%(!.#.>) '
+# export PROMPT='
+# ${SSH_TTY+"%n@%m:"}%3~$(prompt_git_info)$(rvm_info_for_prompt)
+# %(!.#.>) '
 
-# this tests for the presence of rvm 
-# if its loaded, it'll add the prompt
-function rvm_info_for_prompt {
-  ruby_version=$(~/.rvm/bin/rvm-prompt)
-  if [ -n "$ruby_version" ]; then
-    echo " [$ruby_version]"
+export PROMPT='
+${SSH_TTY+"%n@%m:"}%3~
+#%h %(!.#.$) '
+
+export RPROMPT='$(prompt_git_info)$(ruby_prompt_info)'
+
+function ruby_prompt_info {
+  # if [[ -n "$(which rbenv 2>/dev/null)" ]]; then
+  #   ver=$(rbenv_ruby_ver)
+  # else
+    ver=$(rvm_ruby_ver)
+  # fi
+
+  if [ -n "$ver" ]; then
+    echo "  %{${fg_bold[red]}%}★%{$reset_color%} $ver"
   fi
 }
+
+function rvm_ruby_ver {
+  echo "$(~/.rvm/bin/rvm-prompt v p g)"
+}
+
+# function rbenv_ruby_ver {
+#   echo "$(rbenv version | cut -f1 -d ' ')"
+# }
+
+# function vi_mode_info {
+#   if [ -n "$VIMODE" ]; then
+#     if [ "$VIMODE" != "-- INSERT --" ]; then
+#       echo "  $VIMODE"
+#     fi
+#   fi
+# }
+
+# function zle-line-init zle-keymap-select {
+#     VIMODE="${${KEYMAP/vicmd/-- NORMAL --}/(main|viins)/-- INSERT --}"
+#     zle reset-prompt
+# }
+# zle -N zle-line-init
+# zle -N zle-keymap-select
+
+# function precmd_reset_vi_mode {
+#   VIMODE=""
+# }
+# 
+# precmd_functions+='precmd_reset_vi_mode'
