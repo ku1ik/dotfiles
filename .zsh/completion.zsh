27e9b65f154bd938d71e1c14dd2b7415dc9ca0a9
Marcin Kulik <marcin.kulik@gmail.com>
2011-11-05 18:56:30 +0100

    [zsh] improved auto-completion

--- .zsh/completion.zsh
+++ .zsh/completion.zsh
@@ -1,29 +1,55 @@
-# zstyle :compinstall filename "${HOME}/.zshrc"
-
 autoload -U compinit
 compinit -i
 
 zmodload zsh/complist
 
 setopt complete_in_word
+# setopt MENU_COMPLETE # immediatelly insert first match
 
 zstyle ':completion:*' completer _complete _prefix
 zstyle ':completion:*' add-space true
-zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
-# setopt MENU_COMPLETE # immediatelly insert first match
+# Smart case matching && match inside filenames
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+# 'm:{a-z}={A-Z}'
+# zstyle ':completion:*' matcher-list '' '' 'l:|=* r:|=*' 'l:|=* r:|=*'
 
-# Processes
+zstyle ':completion:*:*:*:*:*' menu select
+
+# Rehash when completing commands
+zstyle ":completion:*:commands" rehash 1
+
+# Process completion shows all processes with colors
 zstyle ':completion:*:*:*:*:processes' menu yes select
 zstyle ':completion:*:*:*:*:processes' force-list always
-zstyle ':completion:*:processes' command 'ps -ux'
+zstyle ':completion:*:*:*:*:processes' command 'ps -A -o pid,user,cmd'
+zstyle ':completion:*:*:*:*:processes' list-colors "=(#b) #([0-9]#)*=0=${color[green]}"
+# zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#)*=0=01;31'
 
+# List all processes for killall
+zstyle ':completion:*:processes-names' command "ps -eo cmd= | sed 's:\([^ ]*\).*:\1:;s:\(/[^ ]*/\)::;/^\[/d'"
+
+# No username completion
 zstyle ':completion:*' users off
+
+# Colors in completion
 zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
-# zstyle ':completion:*:descriptions' format '%U%B%d%b%u'
+
+# Display message when no matches are found
 zstyle ':completion:*:warnings' format '%BSorry, no matches for: %d%b'
 
+# Ignore internal zsh functions
+zstyle ':completion:*:functions' ignored-patterns '_*'
+
+# Grouping for completion types (trial)
+zstyle ':completion:*:matches' group 'yes'
+zstyle ':completion:*:descriptions' format "%{${fg_bold[magenta]}%}= %d =%{$reset_color%}"
+# zstyle ':completion:*:descriptions' format '%U%B%d%b%u'
+zstyle ':completion:*' group-name ""
+
+# Speedup path completion
 zstyle ':completion:*' accept-exact '*(N)'
 
-# zstyle ':completion:*:processes-names' command  'ps c -au ${USER} -o command | uniq'
-# zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#)*=0=01;31'
+# Cache expensive completions
+zstyle ':completion:*' use-cache on
+zstyle ':completion:*' cache-path ~/.cache/zsh
