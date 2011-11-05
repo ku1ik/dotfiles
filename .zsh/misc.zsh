e21e4ac1d534820ebab275e337a8be13d7569cba
Marcin Kulik <marcin.kulik@gmail.com>
2011-11-05 18:28:49 +0100

    [zsh] enable REPORTTIME and disable flow controll

--- .zsh/misc.zsh
+++ .zsh/misc.zsh
@@ -4,11 +4,12 @@ zle -N self-insert url-quote-magic
 
 setopt long_list_jobs
 
-setopt correct # correct typos
-
 # edit command line in $EDITOR
 autoload -U edit-command-line
 zle -N edit-command-line
 bindkey "\ee" edit-command-line  # <Esc-e>
 
-eval $(lesspipe)
+export REPORTTIME=60
+
+# disable XON/XOFF flow control (^s/^q)
+stty -ixon
