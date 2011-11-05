378a4492441fa61914848ea919e0fbb4d7fffb6c
Marcin Kulik <marcin.kulik@gmail.com>
2011-11-05 19:03:49 +0100

    [zsh] aliases revisited

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,83 +1,103 @@
+alias reload!='source ~/.zshrc'
+
+# filesystem
 alias ls='ls -ahF --color=auto'
-alias ll='ls -l'
-# alias ll='ls -ltc --color=auto | head'
-# alias lsa='ls -la --color=auto'
+alias ll='ls -la'
+alias lsd='ls -l | grep "^d"' # list dirs
 alias ..='cd ..;' # can then do .. .. .. to move up multiple directories.
 alias ...='.. ..'
 alias cd-='cd;cd -'
+alias df='df -h'
+alias du='du -chs'
+alias md='mkdir -p'
+alias ducks='du -cks * | sort -rn | head -11' # Lists the size of all the folders and files
+
+# processes
 alias psg='ps ax | grep'
 alias psug='ps x | grep'
-alias rtorrent='tmux attach -t rtorrent || tmux new -s rtorrent rtorrent'
-alias tailf='tail -f -n200'
-alias df='df -h'
+
+# opening files
 alias open='xdg-open'
-alias _='sudo'
+alias o='xdg-open'
+alias -s -- jar='java -jar'
 
+# pkg managers
 alias a='sudo apt-get'
+alias y='sudo yum'
+alias yl='yum -C list installed'
+alias ys='yum -C search'
 
-alias v=vim
-alias e=gvim
-alias et='gvim --remote-tab'
-
-alias x='dtrx -v'
-
+# git
 alias g='git'
-alias ga='git add'
-alias gb='git branch'
-alias gc='git commit'
-alias gca='git commit -a'
-alias gd='git diff'
-alias gl='git pull --rebase'
-alias go='git checkout'
-alias gp='git push'
-alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
-alias gs='git status -sb'
 
-alias md='mkdir -p'
+alias ga='g a'
+alias gau='g au'
+alias gaa='g aa'
+# alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
 
-alias b=bundle
+alias gb='g b'
+
+alias gc='g c'
+alias gc-='g c-'
+alias gca='g ca'
+alias gcu='g cu'
+
+alias gs='g s'
+alias gd='g d'
+
+alias gco='g co'
+
+alias gl='git pull'
+alias glr='git pull --rebase'
+alias gp='git push origin HEAD'
+
+# bundler
+alias b='bundle'
 alias be='bundle exec'
-alias rdbm='bundle exec rake db:migrate'
-alias r='bundle exec rails'
-alias rc='bundle exec rails c'
-alias rs='bundle exec rails s thin'
-alias rspec='bundle exec rspec'
-alias bi='bundle install'
+alias bu='bundle update'
 
+# rails
+alias r='rails'
+alias rc='rails c'
+alias rs='rails s'
+alias rg='rails g'
+alias rdbm='rake db:migrate'
+alias rdbmt='RAILS_ENV=test rdbm'
+alias devlog='tail -f -n 300 log/development.log'
+gemcd() { cd "$(gem open -e echo $1)" }
+
+# vagrant
+alias vg='vagrant'
+alias vu='vagrant up'
+
+# editing
+alias v=vim.basic
+alias e=gvim
+
+# find stuff
+alias fd='find . -type d -iname $1'
+alias ff='find . -type f -iname $1'
+alias unsvn='find . -name .svn -print0 | xargs -0 rm -rf'
+
+# other
+alias bc='bc -q'
+alias cls="echo -ne '\033c'"
+alias dl='cd ~/Downloads'
+alias jsonpretty='python -mjson.tool'
 alias man='man -P less'
+alias myip='curl http://whatismyip.org && echo'
+alias profileme="history | awk '{print \$2}' | awk 'BEGIN{FS=\"|\"}{print \$1}' | sort | uniq -c | sort -n | tail -n 20 | sort -nr" # useful command to find what you should be aliasing:
+alias rtorrent='tmux attach -t rtorrent || tmux new -s rtorrent rtorrent'
+alias s='sudo'
 alias sv='sudo service'
+alias t='todo.sh'
+alias tf='tail -f -n200'
+alias x='dtrx -v --one=here'
+alias rot13='tr a-zA-Z n-za-mN-ZA-M'
 
-if $(grc &>/dev/null)
-then
-  alias ping='grc ping'
-  alias diff='grc diff'
-fi
-
+# global aliases
 alias -g G='| grep'
 alias -g L='| less'
 alias -g M='| more'
 alias -g T='| tail'
 alias -g TF='tail -n100 -f'
-
-alias ff='find . -type f -iname $1'
-alias fd='find . -type d -iname $1'
-
-alias t='todo.sh'
-alias tmux='tmux -2'
-alias ducks='du -cks * | sort -rn | head -11' # Lists the size of all the folders and files
-
-# alias jsonpretty='ruby -rap -rjson -e "ap JSON.parse(STDIN.read)"'
-alias jsonpretty='python -mjson.tool'
-
-alias cls="echo -ne '\033c'"
-
-alias dl='cd ~/Downloads'
-
-alias -s -- jar='java -jar'
-
-alias vg=vagrant
-
-alias unsvn='find . -name .svn -print0 | xargs -0 rm -rf'
-
-# useful command to find what you should be aliasing:
-alias profileme="history | awk '{print \$2}' | awk 'BEGIN{FS=\"|\"}{print \$1}' | sort | uniq -c | sort -n | tail -n 20 | sort -nr"
