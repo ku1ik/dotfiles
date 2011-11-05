124fb31e12c956317bb43558081dcea9db476656
Marcin Kulik <marcin.kulik@gmail.com>
2011-11-05 18:26:03 +0100

    .zsh/exports -> .zsh/env

---
+++ .zsh/env.zsh
@@ -0,0 +1,16 @@
+export EDITOR=/usr/bin/vim
+export VISUAL="$EDITOR"
+
+export GREP_OPTIONS='--color=auto'
+export GREP_COLOR='1;32'
+
+export PAGER="less"
+export LESS_TERMCAP_mb=$'\E[01;31m'
+export LESS_TERMCAP_md=$'\E[01;34m'
+export LESS_TERMCAP_me=$'\E[0m'
+export LESS_TERMCAP_se=$'\E[0m'
+export LESS_TERMCAP_so=$'\E[01;44;33m'
+export LESS_TERMCAP_ue=$'\E[0m'
+export LESS_TERMCAP_us=$'\E[01;32m'
+
+export BC_ENV_ARGS=~/.bc
