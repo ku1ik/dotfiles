d93c2d7418489049332aa57e024ba247a080f6bf
Marcin Kulik <marcin.kulik@gmail.com>
2012-05-07 00:35:47 +0200

    [awesome] remove awesome-old

--- .config/awesome-old/hostname.lua
+++
@@ -1,4 +0,0 @@
-local f = io.popen("uname -snr")
-local uname = f:read("*line")
-f:close()
-hostname = string.match(uname, "[%w]+[%s]([%w%p]+)[%s][%w%p]+")
--- .config/awesome-old/local/mkulik.lua
+++
@@ -1,6 +0,0 @@
-function local_setup_tags()
-  -- master factor and slave columns number for "im" tag
-  awful.tag.setmwfact(0.16, tags[1][4])
-  awful.tag.setnmaster(2, tags[1][4])
-  awful.tag.setncol(3, tags[1][4])
-end
