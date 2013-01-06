e2b14cc8454e3194f39ffbd07a71a074e56c4679
Marcin Kulik <marcin.kulik@gmail.com>
2013-01-06 21:55:36 +0100

    [firefox] these tweaks are not needed anymore I suppose

--- firefox/user.js
+++ firefox/user.js
@@ -1,19 +1,2 @@
-// user_pref("network.http.pipelining", true);
-// user_pref("network.http.proxy.pipelining", true);
-// user_pref("network.http.pipelining.maxrequests", 8);
-// user_pref("network.http.pipelining.ssl", true);
 user_pref("network.dns.disableIPv6", true);
-user_pref("network.dnsCacheEntries", 100);
-user_pref("network.dnsCacheExpiration", 600); // keep cached DNS entries for 10 mins
-
-user_pref("content.notify.backoffcount", 5);
-user_pref("content.interrupt.parsing", false);
-
-user_pref("nglayout.initialpaint.delay", 0);
-
-user_pref("browser.history_expire_days", 14);
-user_pref("browser.history_expire_days_min", 7);
-user_pref("browser.display.show_image_placeholders", false);
-user_pref("browser.urlbar.autoFill", true);
-
-user_pref("plugin.expose_full_path", true);
+user_pref("plugins.click_to_play", true);
