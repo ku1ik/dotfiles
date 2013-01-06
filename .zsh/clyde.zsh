382e415bc1f5564d6897587e3a5029c0f16ba52a
Marcin Kulik <marcin.kulik@gmail.com>
2013-01-06 22:00:49 +0100

    [zsh] i don't use clyde anymore

--- .zsh/clyde.zsh
+++
@@ -1,8 +0,0 @@
-clyde() {
-   case $1 in
-       (-Ss | -Si | -Q* | -T | -G)
-           /usr/bin/clyde "$@" ;;
-       (-S* | -R* | -U | *)
-           /usr/bin/sudo /usr/bin/clyde "$@" ;;
-   esac
-}
