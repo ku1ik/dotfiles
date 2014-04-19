393b9289d9c7bcf7dee9d6401adca0105dbb1411
Marcin Kulik <m@ku1ik.com>
2014-04-19 17:30:26 +0200

    [bin] add script for setting keyboard shortcuts in Unity

---
+++ bin/set-unity-keys.sh
@@ -0,0 +1,23 @@
+gsettings set org.gnome.desktop.wm.keybindings close "['<Super>q']"
+
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Super>1']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<Super>2']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['<Super>3']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4 "['<Super>4']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-5 "['<Super>5']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-6 "['<Super>6']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-7 "['<Super>7']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-8 "['<Super>8']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-9 "['<Super>9']"
+
+# gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-1 "['<Shift><Super>1']"
+
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "['<Super>Left']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "['<Super>Right']"
+# gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-left "['<Shift><Super>Left'"
+# gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-right "['<Shift><Super>Right'"
+
+gsettings set com.canonical.Unity.Lenses remote-content-search none
+
+# gsettings set org.gnome.settings-daemon.peripherals.touchpad horiz-scroll-enabled false
+gsettings set org.gnome.desktop.wm.preferences resize-with-right-button true
